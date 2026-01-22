3a949f02be0ea684e58d34feb94f5908d2e3cd29
lorez <125885188+Fuscaturbinado@users.noreply.github.com>
2026-01-22 11:58:39 -0300

    Funcionamento do Login e token

--- SmartShareAPI_python/FluxoModf.py
+++ SmartShareAPI_python/FluxoModf.py
@@ -2,17 +2,21 @@ import requests, logging, json, os, sys, time
 import mimetypes
 from dotenv import load_dotenv
 import pandas as pd
+import json
 
-load_dotenv()
-
+load_dotenv();
 API_BASE = os.getenv("API_BASE")
 API_CLIENT = os.getenv("API_CLIENT")
 API_KEY = os.getenv("API_KEY")
-COD_TAREFA = os.getenv("COD_PROCESSO")
+COD_TAREFA = os.getenv("COD_TAREFA")
 USUARIO = os.getenv("USUARIO")
 SENHA = os.getenv("SENHA")
 AUTENTICACAO = os.getenv("AUTENTICACAO")
-OPCAO = 10;
+TOKEN_CLIENT = os.getenv("TOKEN_CLIENT")
+
+cod_fluxo = input("Qual o código do processo? : ")
+cod_tarefa = input("Avançará qual tarefa? : ")
+OPCAO = input("Opção:");
 
 #Gera Token de acesso para a API SmartShare
 def call_api_login():
@@ -28,18 +32,27 @@ def call_api_login():
     logging.info("Login OK, token recebido")
     return token
 
-token = call_api_login();
+token = call_api_login()
 
-def call_avancar_Fluxo(token, COD_TAREFA):
-    url=f"{API_BASE}";
-    payload ="";
+print(token)
+def avanca_fluxo(token, cod_fluxo, cod_tarefa):
+    url = f"{API_BASE}/api/v2/Fluxo/AvancaFluxo"
+    payload = json.dumps({
+        "lstCamposForm": [],
+        "lstExecutores": [{"cdUsuario": 2}]
+    })
     headers = {
-  'dsCliente': f'{API_CLIENT}/api/v2/Fluxo/AvancaFluxo',
-  'dsChaveAutenticacao': f'{API_KEY}',
-  'tokenUsuario': f'{token}',
-  'cdTarefa': f'{COD_TAREFA}',
-  'cdOpcao': f'{OPCAO}'
+        'dsCliente': API_CLIENT,
+        'dsChaveAutenticacao': AUTENTICACAO,
+        'tokenUsuario': f"{token}",
+        'cdFluxo': cod_fluxo,
+        'cdTarefa': cod_tarefa,
+        'cdOpcao': OPCAO,
+        'Content-Type': 'application/json'
+    }
 
-    };
-    response = requests.request("POST", url, headers=headers, data=payload);
-    return response;
+    response = requests.post(url, headers=headers, data=payload)
+    response.raise_for_status()
+    print("Fluxo avançado com sucesso.")
+response = avanca_fluxo(token, cod_fluxo, cod_tarefa)
+print(response)
